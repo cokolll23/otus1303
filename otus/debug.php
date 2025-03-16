83c11423e10e16f29ad2075d9276afac6513b8e9
ALEX <cavjob@ya.ru>
2025-03-16 08:25:39 +0300

    debug writeToFileFinish1

--- otus/debug.php
+++ otus/debug.php
@@ -1,16 +1,7 @@
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");?>
 <?php
-function writeToLog($data, $title = ''){
-    if (!DEBUG_FILE_NAME)
-        return false;
-    $log = "\n------------------------\n";
-    $log .= date("Y.m.d G:i:s")."\n";
-    $log .= (strlen($title) > 0 ? $title : 'DEBUG')."\n";
-    $log .= print_r($data, 1);
-    $log .= "\n------------------------\n";
-    file_put_contents(__DIR__."/".DEBUG_FILE_NAME, $log, FILE_APPEND);
-    return true;
-}
-
+$currentDate = new DateTime();
+echo $curDate =$currentDate->format('Y-m-d H:i:s');
 $var = [];
-Bitrix\Main\Diag\Debug::writeToFile($var, $varName = '', $fileName = '');
-Bitrix\Main\Diag\Debug::dumpToFile($var, $varName = '', $fileName = '');
+Bitrix\Main\Diag\Debug::writeToFile($curDate, $varName = 'curDateTime', $fileName = '/log.txt');
+//Bitrix\Main\Diag\Debug::dumpToFile($var, $varName = '', $fileName = '');
