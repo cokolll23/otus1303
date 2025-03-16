565b9ad7967d5c7e74134d75400d6e513b1e0e07
ALEX <cavjob@ya.ru>
2025-03-16 21:19:22 +0300

    debug writeToFileFinish1 file_put_contents

--- otus/debug.php
+++ otus/debug.php
@@ -4,4 +4,6 @@ $currentDate = new DateTime();
 echo $curDate =$currentDate->format('Y-m-d H:i:s');
 $var = [];
 Bitrix\Main\Diag\Debug::writeToFile($curDate, $varName = 'curDateTime', $fileName = '/log.txt');
+file_put_contents($_SERVER['DOCUMENT_ROOT'].'/logs/log.txt', $curDate . PHP_EOL, FILE_APPEND);
+
 //Bitrix\Main\Diag\Debug::dumpToFile($var, $varName = '', $fileName = '');
